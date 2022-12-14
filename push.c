8641e6df127e4f3284b6e48a2826fab3ee073d65
Bongani <bonganibaloyi94@gmail.com>
2022-12-14 08:07:46 -0800

    monty

--- push.c
+++ push.c
@@ -29,9 +29,9 @@ void f_push(stack_t **head, unsigned int counter)
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
+		n = atoi(bus.arg);
+		if (bus.lifi == 0)
+			addnode(head, n);
+		else
+			addqueue(head, n);
 }
